124b4ce346c3cec191e2f44501d256ee88ba6ad7
Bryon Epifania <bepifani@mi-l3.msk.21-school.ru>
2022-02-15 19:13:21 +0300

    g

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: bepifani <bepifani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 17:55:52 by bepifani          #+#    #+#             */
-/*   Updated: 2022/02/14 19:26:34 by bepifani         ###   ########.fr       */
+/*   Updated: 2022/02/15 19:00:13 by bepifani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ int	main(int argc, char **argv)
 	ft_init(&sl);
 	sl.win_game = 0;
 	sl.lose = 0;
+	sl.steps = 0;
 	if (argc == 2)
 		ft_make_map(&sl, argv[1]);
 	else
@@ -47,4 +48,5 @@ int	main(int argc, char **argv)
 	mlx_key_hook(sl.win, ft_key, &sl);
 	ft_fill_map(&sl);
 	mlx_loop(sl.mlx);
+	return (0);
 }
